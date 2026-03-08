59b013b2d9003398eccb1f4a69b6c61986b255a7
farhanisty <farhannivtasija@gmail.com>
2026-03-08 22:12:46 +0700

    create login page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tpm_tugas1/theme/app_colors.dart';
 
 class LoginPage extends StatefulWidget{
   const LoginPage({super.key});
@@ -11,12 +12,94 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColors.primaryColor,
       body: SafeArea(
-        child: Text(
-          "hello",
-          style: TextStyle(
-            fontFamily: "Jost"
-          )
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Tugas 1 TPM Teori",
+                style: TextStyle(
+                  fontFamily: "KodeMono",
+                  color: AppColors.secondaryColor,
+                  fontWeight: FontWeight.w700,
+                  fontSize: 12
+                )
+              ),
+              SizedBox(height: 8,),
+              Text(
+                "AUTHENTICATE",
+                style: TextStyle(
+                  fontFamily: "JetBrainsMono",
+                  color: AppColors.secondaryColor,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 48
+                )
+              ),
+              SizedBox(height: 12,),
+              TextField(
+                style: TextStyle(
+                  color: AppColors.secondaryColor
+                ),
+                decoration: InputDecoration(
+                  labelText: "Username",
+                  labelStyle: TextStyle(
+                    color: AppColors.secondaryColor
+                  ),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: AppColors.secondaryColor)
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: AppColors.secondaryColor)
+                  ),
+                ),
+              ),
+              SizedBox(height: 8,),
+              TextField(
+                obscureText: true,
+                style: TextStyle(
+                  color: AppColors.secondaryColor
+                ),
+                decoration: InputDecoration(
+                  labelText: "Password",
+                  labelStyle: TextStyle(
+                    color: AppColors.secondaryColor
+                  ),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: AppColors.secondaryColor)
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: AppColors.secondaryColor)
+                  ),
+                ),
+              ),
+              SizedBox(height: 48,),
+              SizedBox(
+                width: double.infinity,
+                child: OutlinedButton(
+                  onPressed: () => {},
+                  style: OutlinedButton.styleFrom(
+                    backgroundColor: AppColors.secondaryColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8)
+                    )
+                  ),
+                  child: Text(
+                    "Login",
+                    style: TextStyle(
+                      fontFamily: "JetBrainsMono",
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                      color: AppColors.primaryColor
+                    )
+                  ),
+                ),
+              )
+            ],
+          ),
         )
       )
     );
---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primaryColor = Color(0xFF08243F);
+  static const secondaryColor = Color(0xFFFDF1D0);
+}
