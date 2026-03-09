9598aa83f8b0cce8162089349cd9573689280c74
farhanisty <farhannivtasija@gmail.com>
2026-03-10 05:36:52 +0700

    create second menu logic

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: FifthMenuPage(),
+      home: SecondMenuPage(),
     );
   }
 }
